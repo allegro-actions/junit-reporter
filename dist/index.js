62ea9f8d05f358501121b8d77031275df20f40eb
Bartosz Galek <bartosz.galek@allegro.pl>
2021-07-01 11:31:04 +0200

    markdown table formatter introduced #80

--- dist/index.js
+++ dist/index.js
@@ -435,6 +435,7 @@ function getResult(testCase) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.toMarkdown = void 0;
 const suites_1 = __nccwpck_require__(5597);
+const markdown_table_1 = __nccwpck_require__(6335);
 function getMessage(testCase) {
     var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
     return (_k = (((_c = (_b = (_a = testCase.skipped) === null || _a === void 0 ? void 0 : _a[0]) === null || _b === void 0 ? void 0 : _b.message) === null || _c === void 0 ? void 0 : _c.trim()) ||
@@ -513,7 +514,7 @@ function getMessageAboutLimit(results, count) {
 function toMarkdown(report) {
     var _a, _b;
     if (!report.hasTests()) {
-        return '### Test results not found\n';
+        return '# Test results not found\n';
     }
     const results = new Map();
     suites_1.forEachTestCase(report.getTestSuites(), (testCase) => {
@@ -523,11 +524,19 @@ function toMarkdown(report) {
             return;
         const name = getName(testCase);
         const message = getMessage(testCase);
-        const details = `<details>\n <summary>${name}</summary>\n\n${message}\n\n</details>`;
+        const details = `<details>\n<summary>${name}</summary>\n\n${message}\n\n</details>`;
         results.set(type, (results.get(type) || []).concat(details));
     });
     const tests = report.counter.tests, successful = report.counter.succesfull;
-    let result = `### Found ${tests} ${getPlural('tests', tests)}\n`;
+    let result = `# Results (${tests} ${getPlural('tests', tests)}) ${successful === tests ? '✓' : '✗'}\n`;
+    report.getTestSuites().forEach((suite) => {
+        result += `\n## ${suite.name}\n\n`;
+        const testCases = suite.testcase || [];
+        const rows = testCases.map(it => [`${it.name}`, `${it.failure || it.error ? '×' : it.skipped ? '💔' : '✓'}`, `${(it.time || 0) * 1000} ms`]);
+        const tableHeader = ['Test case', 'Result', 'Duration'];
+        result += markdown_table_1.markdownTable([tableHeader].concat(rows), { align: ['l', 'c', 'c'] });
+        result += '\n';
+    });
     if (successful === tests) {
         result += '\n- **All** tests were successful';
     }
@@ -553,7 +562,7 @@ function toMarkdown(report) {
     for (const key of keys) {
         if ((((_a = results.get(key)) === null || _a === void 0 ? void 0 : _a.length) || 0) <= 0)
             continue;
-        result += `\n### ${getTitle(key)}\n`;
+        result += `\n\n## ${getTitle(key)}\n`;
         result += getMessageAboutLimit(results.get(key), report.counter.get(key));
         result += '\n';
         result += ((_b = results.get(key)) === null || _b === void 0 ? void 0 : _b.join('\n')) || '';
@@ -627,6 +636,218 @@ function run() {
 }
 exports.run = run;
 run();
+
+
+/***/ }),
+
+/***/ 6335:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.markdownTable = void 0;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function markdownTable(table, options = {}) {
+    const settings = options || {};
+    const align = (settings.align || []).concat();
+    const stringLength = settings.stringLength || defaultStringLength;
+    /** @type {number[]} Character codes as symbols for alignment per column. */
+    const alignments = [];
+    let rowIndex = -1;
+    /** @type {string[][]} Cells per row. */
+    const cellMatrix = [];
+    /** @type {number[][]} Sizes of each cell per row. */
+    const sizeMatrix = [];
+    /** @type {number[]} */
+    const longestCellByColumn = [];
+    let mostCellsPerRow = 0;
+    /** @type {number} */
+    let columnIndex;
+    /** @type {string[]} Cells of current row */
+    let row;
+    /** @type {number[]} Sizes of current row */
+    let sizes;
+    /** @type {number} Sizes of current cell */
+    let size;
+    /** @type {string} Current cell */
+    let cell;
+    /** @type {string[]} Chunks of current line. */
+    let line;
+    /** @type {string} */
+    let before;
+    /** @type {string} */
+    let after;
+    /** @type {number} */
+    let code;
+    // This is a superfluous loop if we don’t align delimiters, but otherwise we’d
+    // do superfluous work when aligning, so optimize for aligning.
+    while (++rowIndex < table.length) {
+        columnIndex = -1;
+        row = [];
+        sizes = [];
+        if (table[rowIndex].length > mostCellsPerRow) {
+            mostCellsPerRow = table[rowIndex].length;
+        }
+        while (++columnIndex < table[rowIndex].length) {
+            cell = serialize(table[rowIndex][columnIndex]);
+            if (settings.alignDelimiters !== false) {
+                size = stringLength(cell);
+                sizes[columnIndex] = size;
+                if (longestCellByColumn[columnIndex] === undefined ||
+                    size > longestCellByColumn[columnIndex]) {
+                    longestCellByColumn[columnIndex] = size;
+                }
+            }
+            row.push(cell);
+        }
+        cellMatrix[rowIndex] = row;
+        sizeMatrix[rowIndex] = sizes;
+    }
+    // Figure out which alignments to use.
+    columnIndex = -1;
+    if (typeof align === 'object' && 'length' in align) {
+        while (++columnIndex < mostCellsPerRow) {
+            alignments[columnIndex] = toAlignment(align[columnIndex]);
+        }
+    }
+    else {
+        code = toAlignment(align);
+        while (++columnIndex < mostCellsPerRow) {
+            alignments[columnIndex] = code;
+        }
+    }
+    // Inject the alignment row.
+    columnIndex = -1;
+    row = [];
+    sizes = [];
+    while (++columnIndex < mostCellsPerRow) {
+        code = alignments[columnIndex];
+        before = '';
+        after = '';
+        if (code === 99 /* `c` */) {
+            before = ':';
+            after = ':';
+        }
+        else if (code === 108 /* `l` */) {
+            before = ':';
+        }
+        else if (code === 114 /* `r` */) {
+            after = ':';
+        }
+        // There *must* be at least one hyphen-minus in each alignment cell.
+        size =
+            settings.alignDelimiters === false
+                ? 1
+                : Math.max(1, longestCellByColumn[columnIndex] - before.length - after.length);
+        cell = before + '-'.repeat(size) + after;
+        if (settings.alignDelimiters !== false) {
+            size = before.length + size + after.length;
+            if (size > longestCellByColumn[columnIndex]) {
+                longestCellByColumn[columnIndex] = size;
+            }
+            sizes[columnIndex] = size;
+        }
+        row[columnIndex] = cell;
+    }
+    // Inject the alignment row.
+    cellMatrix.splice(1, 0, row);
+    sizeMatrix.splice(1, 0, sizes);
+    rowIndex = -1;
+    /** @type {string[]} */
+    const lines = [];
+    while (++rowIndex < cellMatrix.length) {
+        row = cellMatrix[rowIndex];
+        sizes = sizeMatrix[rowIndex];
+        columnIndex = -1;
+        line = [];
+        while (++columnIndex < mostCellsPerRow) {
+            cell = row[columnIndex] || '';
+            before = '';
+            after = '';
+            if (settings.alignDelimiters !== false) {
+                size = longestCellByColumn[columnIndex] - (sizes[columnIndex] || 0);
+                code = alignments[columnIndex];
+                if (code === 114 /* `r` */) {
+                    before = ' '.repeat(size);
+                }
+                else if (code === 99 /* `c` */) {
+                    if (size % 2) {
+                        before = ' '.repeat(size / 2 + 0.5);
+                        after = ' '.repeat(size / 2 - 0.5);
+                    }
+                    else {
+                        before = ' '.repeat(size / 2);
+                        after = before;
+                    }
+                }
+                else {
+                    after = ' '.repeat(size);
+                }
+            }
+            if (settings.delimiterStart !== false && !columnIndex) {
+                line.push('|');
+            }
+            if (settings.padding !== false &&
+                // Don’t add the opening space if we’re not aligning and the cell is
+                // empty: there will be a closing space.
+                !(settings.alignDelimiters === false && cell === '') &&
+                (settings.delimiterStart !== false || columnIndex)) {
+                line.push(' ');
+            }
+            if (settings.alignDelimiters !== false) {
+                line.push(before);
+            }
+            line.push(cell);
+            if (settings.alignDelimiters !== false) {
+                line.push(after);
+            }
+            if (settings.padding !== false) {
+                line.push(' ');
+            }
+            if (settings.delimiterEnd !== false ||
+                columnIndex !== mostCellsPerRow - 1) {
+                line.push('|');
+            }
+        }
+        lines.push(settings.delimiterEnd === false
+            ? line.join('').replace(/ +$/, '')
+            : line.join(''));
+    }
+    return lines.join('\n');
+}
+exports.markdownTable = markdownTable;
+/**
+ * @param {string|null|undefined} [value]
+ * @returns {string}
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function serialize(value) {
+    return value === null || value === undefined ? '' : String(value);
+}
+/**
+ * @param {string} value
+ * @returns {number}
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function defaultStringLength(value) {
+    return value.length;
+}
+/**
+ * @param {string|null|undefined} value
+ * @returns {number}
+ */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function toAlignment(value) {
+    const code = typeof value === 'string' ? value.charCodeAt(0) : 0;
+    return code === 67 /* `C` */ || code === 99 /* `c` */
+        ? 99 /* `c` */
+        : code === 76 /* `L` */ || code === 108 /* `l` */
+            ? 108 /* `l` */
+            : code === 82 /* `R` */ || code === 114 /* `r` */
+                ? 114 /* `r` */
+                : 0;
+}
 
 
 /***/ }),
